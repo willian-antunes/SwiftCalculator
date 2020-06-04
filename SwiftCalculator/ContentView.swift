3426bef8bc4e1c2e4e7d5460bd815b32e8adf3d5
Willian Antunes <willian.antunes@me.com>
2020-06-04 18:09:44 -0300

    Initial Commit

---
+++ SwiftCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCalculator
+//
+//  Created by Willian Antunes on 04/06/20.
+//  Copyright © 2020 Willian Antunes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
